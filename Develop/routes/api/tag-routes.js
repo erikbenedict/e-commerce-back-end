e144cde250fb68d1e5e28477199b437eda10c997
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-06-17 13:13:28 -0400

    feature: added GET routes for '/' and '/:id' in 'tag-routes.js'

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,16 +1,23 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
+// * The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{ model: Product }],
+  })
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    include: [{ model: Product }],
+    where: { id: req.params.id },
+  })
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.post('/', (req, res) => {
