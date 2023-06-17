2db06027a682317b129de667cab9d98cf4b63fff
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-06-17 13:16:58 -0400

    feature: added DELETE route for '/:id' in 'tag-routes.js'

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -35,7 +35,11 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: { id: req.params.id },
+  })
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(400).json(err));
 });
 
 module.exports = router;
