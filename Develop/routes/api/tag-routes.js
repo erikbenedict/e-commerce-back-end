f5f46c64c0cc33c0390cb0e428894c0582a5dfe5
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-06-17 13:16:12 -0400

    feature: added POST route for '/', and PUT route for '/:id' in 'tag-routes.js'

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -21,11 +21,17 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  Tag.create(req.body)
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: { id: req.params.id },
+  })
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.delete('/:id', (req, res) => {
