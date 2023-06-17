9da1c461eef81481fb4a1694c03c14008b18a044
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-06-16 22:33:42 -0400

    feature: added GET routes for '/' and '/:id' in 'category-routes.js'

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,16 +1,23 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
+// * The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    include: [{ model: Product }],
+  })
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    include: [{ model: Product }],
+    where: { id: req.params.id },
+  })
+    .then((data) => res.status(200).json(data))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.post('/', (req, res) => {
