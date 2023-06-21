b7f5b4f64119140357d051e89f99a142b3ee0c66
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-06-20 21:16:36 -0400

    chore: added comments to 'tag-routes.js'

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// * The `/api/tags` endpoint
+// **** The `/api/tags` endpoint ****
 
+// * Get all tags
 router.get('/', (req, res) => {
   Tag.findAll({
     include: [{ model: Product }],
@@ -11,6 +12,7 @@ router.get('/', (req, res) => {
     .catch((err) => res.status(400).json(err));
 });
 
+// * Get one tag
 router.get('/:id', (req, res) => {
   Tag.findOne({
     include: [{ model: Product }],
@@ -20,12 +22,14 @@ router.get('/:id', (req, res) => {
     .catch((err) => res.status(400).json(err));
 });
 
+// * Create new tag
 router.post('/', (req, res) => {
   Tag.create(req.body)
     .then((data) => res.status(200).json(data))
     .catch((err) => res.status(400).json(err));
 });
 
+// * Update tag
 router.put('/:id', (req, res) => {
   Tag.update(req.body, {
     where: { id: req.params.id },
@@ -34,6 +38,7 @@ router.put('/:id', (req, res) => {
     .catch((err) => res.status(400).json(err));
 });
 
+// * Delete tag
 router.delete('/:id', (req, res) => {
   Tag.destroy({
     where: { id: req.params.id },
